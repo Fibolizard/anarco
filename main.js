84152e9af168f4a797e88dec3612ecdb883d1dde
SirDrazilobif <63016700+Fibolizard@users.noreply.github.com>
2023-12-09 16:26:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const socketio = require('socket.io');
+
+http.createServer(app);
 app.use(express.static('public'));
 app.listen(3000, () =>{
 console.log("server on port 3000");
